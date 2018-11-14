9909a1727b75b65fa6009a38085e857c46040305
Medard Ilunga <medard.ilunga@outlook.com>
2018-11-14 12:59:37 +0200

    Update formView

--- src/Requests/Types/FormView.php
+++ src/Requests/Types/FormView.php
@@ -10,8 +10,31 @@ namespace Foundry\Requests\Types;
  */
 class FormView{
 
+    /**
+     * form rows
+     * @var array
+     */
     protected $rows;
+    /**
+     * form name
+     * @var string
+     */
     protected $name;
+    /**
+     * Form POST url
+     * @var string
+     */
+    protected $postUrl;
+    /**
+     * Form PUT url
+     * @var string
+     */
+    protected $putUrl;
+    /**
+     * Form DELETE url
+     * @var string
+     */
+    protected $deleteUrl;
 
     /**
      * FormView constructor.
@@ -25,6 +48,76 @@ class FormView{
         $this->rows = $rows;
     }
 
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPostUrl()
+    {
+        return $this->postUrl;
+    }
+
+    /**
+     * @param string $postUrl
+     */
+    public function setPostUrl($postUrl)
+    {
+        $this->postUrl = $postUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPutUrl()
+    {
+        return $this->putUrl;
+    }
+
+    /**
+     * @param string $putUrl
+     */
+    public function setPutUrl($putUrl)
+    {
+        $this->putUrl = $putUrl;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDeleteUrl()
+    {
+        return $this->deleteUrl;
+    }
+
+    /**
+     * @param string $deleteUrl
+     */
+    public function setDeleteUrl($deleteUrl)
+    {
+        $this->deleteUrl = $deleteUrl;
+    }
+
+    public function setFormUrls($postUrl = '', $putUrl = '', $deleteUrl = '')
+    {
+        $this->setPostUrl($postUrl);
+        $this->setPutUrl($putUrl);
+        $this->setDeleteUrl($deleteUrl);
+    }
 
     /**
      * Add a form row
@@ -47,6 +140,16 @@ class FormView{
     public function jsonSerialize()
     {
         $r = array();
+        $u = array();
+
+        if ($this->getPostUrl())
+            $u['POST'] = $this->getPostUrl();
+
+        if($this->getPutUrl())
+            $u['PUT'] = $this->getPutUrl();
+
+        if($this->getDeleteUrl())
+            $u['DELETE'] = $this->getDeleteUrl();
 
         /**
          * @var $row FormRow
@@ -57,6 +160,7 @@ class FormView{
 
         $json = array(
             'name' => $this->name,
+            'methods' => (array) $u,
             'rows' => (array) $r
         );
 
