f7164a3c78fd84425a413e75ee1c5fdcbd8cdc1f
Greg Gunner <greg@co-foundry.co.za>
2018-12-03 12:00:27 +0200

    Updating the TextType to supplrt a multi line option

--- src/Requests/Types/TextType.php
+++ src/Requests/Types/TextType.php
@@ -10,4 +10,16 @@ namespace Foundry\Requests\Types;
  */
 class TextType extends Type{
 
+	protected $multiline = false;
+
+	public function setMultiline($state)
+	{
+		$this->multiline = $state;
+		return $this;
+	}
+
+	public function isMultiline()
+	{
+		return $this->multiline;
+	}
 }
