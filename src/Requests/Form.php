97c3367a979c5e6936ed2e063d59aa477abff9e8
Medard Ilunga <medard.ilunga@outlook.com>
2018-12-18 09:36:31 +0200

    Add functionality to handle nested forms

--- src/Requests/Form.php
+++ src/Requests/Form.php
@@ -14,144 +14,159 @@ use Illuminate\Support\MessageBag;
 
 abstract class Form
 {
-	/**
-	 * @var array form inputs
-	 */
+    /**
+     * @var array form inputs
+     */
     protected $inputs = [];
 
-	/**
-	 * @var Response The response received after calling the hanlde method
-	 */
+    /**
+     * @var Response The response received after calling the hanlde method
+     */
     protected $response;
 
-	/**
-	 * @var \Closure The on success handler
-	 */
+    /**
+     * @var \Closure The on success handler
+     */
     protected $onSuccess;
 
-	/**
-	 * @var \Closure The on error handler
-	 */
-	protected $onError;
+    /**
+     * @var \Closure The on error handler
+     */
+    protected $onError;
 
-	/**
-	 * @var string The view for rendering the form
-	 */
-	protected $view;
+    /**
+     * @var string The view for rendering the form
+     */
+    protected $view;
 
-	/**
-	 * @var string The action url to submit to
-	 */
-	protected $action;
+    /**
+     * @var string The action url to submit to
+     */
+    protected $action;
 
-	/**
-	 * @var string POST, GET, PUT, DELETE
-	 */
-	protected $method;
+    /**
+     * @var string POST, GET, PUT, DELETE
+     */
+    protected $method;
 
-	/**
-	 * @var Model The associated model instance
-	 */
-	protected $model;
+    /**
+     * @var Model The associated model instance
+     */
+    protected $model;
 
-	/**
-	 * @var array The loaded rules
-	 */
-	protected $rules;
+    /**
+     * @var array The loaded rules
+     */
+    protected $rules;
 
-	protected $service;
-	
-	protected $request;
+    protected $service;
 
-	public function __construct($inputs)
+    protected $request;
+
+    public function __construct($inputs)
     {
         $this->setInputs($inputs);
         $this->rules = static::rules();
     }
 
-	/**
-	 * Set the inputs
-	 *
-	 * @param $inputs
-	 *
-	 * @return $this
-	 */
+    /**
+     * Set the inputs
+     *
+     * @param $inputs
+     *
+     * @return $this
+     */
     public function setInputs($inputs)
     {
-    	$this->inputs = $inputs;
-    	return $this;
+        $this->inputs = $inputs;
+        return $this;
     }
 
-	/**
-	 * Get the inputs
-	 *
-	 * @return array
-	 */
+    /**
+     * Get the inputs
+     *
+     * @return array
+     */
     public function getInputs()
     {
-    	return $this->inputs;
+        return $this->inputs;
     }
 
-	/**
-	 * @param Request $request
-	 *
-	 * @return Form
-	 */
+    /**
+     * @param Request $request
+     *
+     * @param null $id
+     * @return Form
+     */
     static public function fromRequest(Request $request, $id = null)
     {
-    	$form = new static($request->only(static::fields()));
-    	$form->setRequest($request);
-    	if ($id && $model = static::model($id)) {
-		    $form->setModel($model);
-	    }
-    	return $form;
+        $fields = $request->only(static::getFormView()->getName());
+
+        $fields = sizeof($fields) === 0? $request->input() : $fields[static::getFormView()->getName()];
+
+        $form = new static(static::only(static::fields(), $fields));
+        $form->setRequest($request);
+        if ($id && $model = static::model($id)) {
+            $form->setModel($model);
+        }
+        return $form;
     }
 
-	/**
-	 * Handle the request
-	 *
-	 * @param Request $request
-	 * @param $id
-	 *
-	 * @return Response
-	 */
+    /**
+     * Handle the request
+     *
+     * @param Request $request
+     * @param $id
+     *
+     * @return Response
+     */
     static public function handleRequest(Request $request, $id = null)
     {
-    	$form = static::fromRequest($request, $id);
-    	return $form->handle();
+        $form = static::fromRequest($request, $id);
+        return $form->handle();
     }
 
-	/**
-	 * Set the request
-	 *
-	 * @param $request
-	 *
-	 * @return $this
-	 */
+    /**
+     * Set the request
+     *
+     * @param $request
+     *
+     * @return $this
+     */
     public function setRequest($request)
     {
-    	$this->request = $request;
-	    return $this;
+        $this->request = $request;
+        return $this;
     }
 
-	/**
-	 * Set the rules
-	 *
-	 * @param $rules
-	 */
+    /**
+     * Get the request
+     *
+     * @return mixed
+     */
+    public function getRequest()
+    {
+        return $this->request;
+    }
+
+    /**
+     * Set the rules
+     *
+     * @param $rules
+     */
     public function setRules($rules)
     {
-    	$this->rules = $rules;
+        $this->rules = $rules;
     }
 
-	/**
-	 * Get the set rules
-	 *
-	 * @return array
-	 */
+    /**
+     * Get the set rules
+     *
+     * @return array
+     */
     public function getRules()
     {
-    	return $this->rules;
+        return $this->rules;
     }
 
     /**
@@ -187,66 +202,66 @@ abstract class Form
      * @return array
      */
     public function messages() {
-    	return [];
+        return [];
     }
 
-	/**
-	 * Returns the service for this form
-	 *
-	 * @return Service
-	 */
+    /**
+     * Returns the service for this form
+     *
+     * @return Service
+     */
     static abstract function service();
 
-	/**
-	 * Gets the form view object for rendering the form
-	 *
-	 * @param int $id
-	 * 
-	 * @return FormView
-	 */
+    /**
+     * Gets the form view object for rendering the form
+     *
+     * @param int $id
+     *
+     * @return FormView
+     */
     static abstract function getFormView($id = null): FormView;
 
-	/**
-	 * Get the model for a given id
-	 *
-	 * @param $id
-	 *
-	 * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|null|object
-	 */
+    /**
+     * Get the model for a given id
+     *
+     * @param $id
+     *
+     * @return \Illuminate\Database\Eloquent\Builder|\Illuminate\Database\Eloquent\Model|null|object
+     */
     static public function model($id)
     {
-    	return call_user_func([static::service(), 'model'], $id);
+        return call_user_func([static::service(), 'model'], $id);
     }
 
-	/**
-	 * Set the types to cast inputs to
-	 *
-	 * @see http://php.net/manual/en/function.settype.php
-	 * @return array
-	 */
+    /**
+     * Set the types to cast inputs to
+     *
+     * @see http://php.net/manual/en/function.settype.php
+     * @return array
+     */
     static public function types()
     {
-    	return [];
-    }
-    
-	/**
-	 * Set the model to use with this form Form Request
-	 */
-    public function setModel(Model $model): Form
-    {
-    	$this->model = $model;
-    	$this->setRules(static::rules($model));
-    	return $this;
+        return [];
     }
 
-	/**
-	 * Get the associated model
-	 *
-	 * @return mixed
-	 */
+    /**
+     * Set the model to use with this form Form Request
+     */
+    public function setModel(Model $model): Form
+    {
+        $this->model = $model;
+        $this->setRules(static::rules($model));
+        return $this;
+    }
+
+    /**
+     * Get the associated model
+     *
+     * @return mixed
+     */
     public function getModel()
     {
-    	return $this->model;
+        return $this->model;
     }
 
     /**
@@ -257,9 +272,9 @@ abstract class Form
      */
     public function validate($rules = null)
     {
-    	if ($rules === null) {
-		    $rules = $this->getRules();
-	    }
+        if ($rules === null) {
+            $rules = $this->getRules();
+        }
         if($this->authorize()){
             $validator = Validator::make($this->inputs, $rules, $this->messages());
             if ($validator->fails()) {
@@ -272,225 +287,226 @@ abstract class Form
         }
     }
 
-	/**
-	 * Extract only the wanted keys from the input
-	 *
-	 * @param $keys
-	 *
-	 * @return array
-	 */
-    public function only($keys)
+    /**
+     * Extract only the wanted keys from the inputs
+     *
+     * @param $keys
+     *
+     * @return array
+     */
+    static function only($keys, $inputs)
     {
-	    $results = [];
+        $results = [];
 
-	    $placeholder = new \stdClass();
+        $placeholder = new \stdClass();
 
-	    foreach (is_array($keys) ? $keys : func_get_args() as $key) {
-		    $value = data_get($this->inputs, $key, $placeholder);
+        foreach (is_array($keys) ? $keys : [$keys] as $key) {
+            $value = data_get($inputs, $key, $placeholder);
 
-		    if ($value !== $placeholder) {
-			    Arr::set($results, $key, $value);
-		    }
-	    }
+            if ($value !== $placeholder) {
+                Arr::set($results, $key, $value);
+            }
+        }
 
-	    return $results;
+        return $results;
     }
 
-	/**
-	 * Converst the values to their typed equivilents
-	 *
-	 * @param $values
-	 *
-	 * @return mixed
-	 */
+    /**
+     * Converst the values to their typed equivilents
+     *
+     * @param $values
+     *
+     * @return mixed
+     */
     static public function typedValues($values)
     {
-    	$cast = static::types();
-    	foreach ($values as $key => $value) {
-    		if (isset($cast[$key])) {
-    			if ($value === "" || $value === null) {
-				    $values[$key] = null;
-			    } else {
-				    settype($values[$key], $cast[$key]);
-			    }
-		    }
-	    }
-	    return $values;
+        $cast = static::types();
+        foreach ($values as $key => $value) {
+            if (isset($cast[$key])) {
+                if ($value === "" || $value === null) {
+                    $values[$key] = null;
+                } else {
+                    settype($values[$key], $cast[$key]);
+                }
+            }
+        }
+        return $values;
     }
 
-	/**
-	 * Extract only the wanted keys from the input
-	 *
-	 * @param $keys
-	 *
-	 * @return array
-	 */
-	public function except($keys)
-	{
-		return array_except($this->inputs, $keys);
-	}
+    /**
+     * Extract only the wanted keys from the input
+     *
+     * @param $keys
+     *
+     * @return array
+     */
+    public function except($keys)
+    {
+        return array_except($this->inputs, $keys);
+    }
 
-	/**
-	 * Handle the form request
-	 *
-	 * @return Response
-	 */
-	abstract public function handle() : Response;
-	
-	/**
-	 * Handle the form using the given service
-	 *
-	 * @param string $service The Service class name to the use
-	 * @param string $method The static method to call against the service class
-	 *
-	 * @return mixed|Response|View
-	 */
+    /**
+     * Handle the form request
+     *
+     * @return Response
+     */
+    abstract public function handle() : Response;
+
+    /**
+     * Handle the form using the given service
+     *
+     * @param string $service The Service class name to the use
+     * @param string $method The static method to call against the service class
+     *
+     * @return mixed|Response|View
+     */
     protected function response($response)
     {
-	    $this->setResponse($response);
-    	if ($this->response->isSuccess()) {
-    		return $this->handleOnSuccess();
-	    } else {
-    		return $this->handleOnError();
-	    }
+        $this->setResponse($response);
+        if ($this->response->isSuccess()) {
+            return $this->handleOnSuccess();
+        } else {
+            return $this->handleOnError();
+        }
     }
 
-	/**
-	 * Set the foundry response object
-	 *
-	 * @param Response $response
-	 *
-	 * @return $this
-	 */
+    /**
+     * Set the foundry response object
+     *
+     * @param Response $response
+     *
+     * @return $this
+     */
     public function setResponse($response)
     {
-    	$this->response = $response;
-    	return $this;
+        $this->response = $response;
+        return $this;
     }
 
-	/**
-	 * Get the foundry response object
-	 *
-	 * @return Response
-	 */
+    /**
+     * Get the foundry response object
+     *
+     * @return Response
+     */
     public function getResponse()
     {
-    	return $this->response;
+        return $this->response;
     }
 
-	/**
-	 * Set the view for rendering this form
-	 *
-	 * @param $view
-	 *
-	 * @return Form
-	 */
+    /**
+     * Set the view for rendering this form
+     *
+     * @param $view
+     *
+     * @return Form
+     */
     public function setView($view) : Form
     {
-    	$this->view = $view;
-    	return $this;
+        $this->view = $view;
+        return $this;
     }
 
-	/**
-	 * Set the on Success handler
-	 *
-	 * @param \Closure $closure
-	 *
-	 * @return Form
-	 */
+    /**
+     * Set the on Success handler
+     *
+     * @param \Closure $closure
+     *
+     * @return Form
+     */
     public function onSuccess(\Closure $closure) : Form
     {
-    	$this->onSuccess = $closure;
-	    return $this;
+        $this->onSuccess = $closure;
+        return $this;
     }
 
-	/**
-	 * @return mixed|Response|View
-	 */
-	protected function handleOnSuccess()
+    /**
+     * @return mixed|Response|View
+     */
+    protected function handleOnSuccess()
     {
-    	if (isset($this->onSuccess) && is_callable($this->onSuccess)) {
-    		return call_user_func($this->onSuccess, $this);
-	    } else {
-    		return $this->response;
-	    }
+        if (isset($this->onSuccess) && is_callable($this->onSuccess)) {
+            return call_user_func($this->onSuccess, $this);
+        } else {
+            return $this->response;
+        }
     }
 
-	/**
-	 * Set the on Error handling
-	 *
-	 * @param \Closure $closure
-	 *
-	 * @return Form
-	 */
-	public function onError(\Closure $closure) : Form
-	{
-		$this->onError = $closure;
-		return $this;
-	}
+    /**
+     * Set the on Error handling
+     *
+     * @param \Closure $closure
+     *
+     * @return Form
+     */
+    public function onError(\Closure $closure) : Form
+    {
+        $this->onError = $closure;
+        return $this;
+    }
 
-	/**
-	 * Handle On Error
-	 *
-	 * @return mixed|Response|View
-	 */
-	protected function handleOnError()
-	{
-		if (isset($this->onError) && is_callable($this->onError)) {
-			$return = call_user_func($this->onError, $this);
-			if ($return instanceof RedirectResponse) {
-				$error = $this->response->getError();
-				if (is_string($error)) {
-					$return->with('status', $error);
-				} elseif (is_array($error)) {
-					$return->withErrors($error, 'form');
-				}
-			}
-			return $return;
-		} elseif (!empty($this->view)) {
-			$form = $this->getFormView();
-			$form
-				->setAction($this->action)
-				->setMethod($this->method)
-			;
-			$error = $this->response->getError();
-			if ($this->response->getCode() === 422) {
-				$form->setErrors(new MessageBag($error));
-			} elseif(is_string($error)) {
-				app('session')->flash('status', $error);
-			}
-			return view($this->view, [
-				'form' => $form
-			]);
-		} else {
-			return $this->response;
-		}
-	}
+    /**
+     * Handle On Error
+     *
+     * @return mixed|Response|View
+     */
+    protected function handleOnError()
+    {
+        if (isset($this->onError) && is_callable($this->onError)) {
+            $return = call_user_func($this->onError, $this);
+            if ($return instanceof RedirectResponse) {
+                $error = $this->response->getError();
+                if (is_string($error)) {
+                    $return->with('status', $error);
+                } elseif (is_array($error)) {
+                    $return->withErrors($error, 'form');
+                }
+            }
+            return $return;
+        } elseif (!empty($this->view)) {
+            $form = $this->getFormView();
+            $form
+                ->setAction($this->action)
+                ->setMethod($this->method)
+            ;
+            $error = $this->response->getError();
+            if ($this->response->getCode() === 422) {
+                $form->setErrors(new MessageBag($error));
+            } elseif(is_string($error)) {
+                app('session')->flash('status', $error);
+            }
+            return view($this->view, [
+                'form' => $form
+            ]);
+        } else {
+            return $this->response;
+        }
+    }
 
-	/**
-	 * Set the form action
-	 *
-	 * @param $action
-	 *
-	 * @return $this
-	 */
-	public function setAction($action)
-	{
-		$this->action = $action;
-		return $this;
-	}
 
-	/**
-	 * Set the method of the form
-	 *
-	 * @param $method
-	 *
-	 * @return $this
-	 */
-	public function setMethod($method)
-	{
-		$this->method = $method;
-		return $this;
-	}
+    /**
+     * Set the form action
+     *
+     * @param $action
+     *
+     * @return $this
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+        return $this;
+    }
+
+    /**
+     * Set the method of the form
+     *
+     * @param $method
+     *
+     * @return $this
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+        return $this;
+    }
 
 }
