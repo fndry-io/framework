fee10cf607e0229bfa12d097cad7e2fb8aba58dd
Greg Gunner <greg@co-foundry.co.za>
2019-01-17 09:26:42 +0200

    Correcting the reference type

--- src/Requests/Types/ReferenceInputType.php
+++ src/Requests/Types/ReferenceInputType.php
@@ -2,8 +2,7 @@
 
 namespace Foundry\Requests\Types;
 
-use Foundry\Requests\Types\Traits\HasQueryOptions;
-use Foundry\Requests\Types\Traits\HasRequest;
+use Foundry\Requests\Types\Traits\HasButtons;
 
 
 /**
@@ -15,8 +14,7 @@ use Foundry\Requests\Types\Traits\HasRequest;
  */
 class ReferenceInputType extends ChoiceInputType {
 
-	use HasRequest;
-	use HasQueryOptions;
+	use HasButtons;
 
 	/**
 	 * Reference constructor
@@ -24,42 +22,25 @@ class ReferenceInputType extends ChoiceInputType {
 	 * @param string $name The field name
 	 * @param string $label
 	 * @param array $options The options to display if not doing a url fetch for them
-	 * @param string $url The url to fetch the list of available options. If this and options are null, then the Type the equivalent to a hasOne relationship
-	 * @param bool $multiple
 	 * @param bool $required
-	 * @param string $request_url
-	 * @param string $request_name
-	 * @param string $request_id
 	 * @param null $value
 	 * @param string $position
 	 * @param string|null $rules
 	 * @param string|null $id
 	 * @param string|null $placeholder
-	 * @param string $query_param
 	 */
 	public function __construct(
 		string $name,
 		string $label,
 		bool $required = true,
 		array $options,
-		?string $url,
-		bool $multiple,
-		string $request_url,
-		string $request_name,
-		string $request_id = null,
 		$value = null,
 		string $position = 'full',
 		string $rules = null,
 		string $id = null,
-		string $placeholder = null,
-		string $query_param = 'q'
+		string $placeholder = null
 	) {
-		parent::__construct( $name, $label, $required, false, $multiple, $options, $value, $position, $rules, $id, $placeholder );
-		$this->setUrl( $url );
-		$this->setQueryParam( $query_param );
-		$this->setRequestUrl( $request_url );
-		$this->setRequestName( $request_name );
-		$this->setRequestId( $request_id );
+		parent::__construct( $name, $label, $required, false, false, $options, $value, $position, $rules, $id, $placeholder );
 		$this->setType( 'reference' );
 	}
 
