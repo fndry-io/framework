5d47b08663f330fedcf0943e8e576a8ff4ee2988
Greg Gunner <greg@co-foundry.co.za>
2019-02-27 19:34:34 +0200

    Adding in full text search trait for Models

---
+++ src/Models/Traits/FullTextSearch.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace Foundry\Models\Traits;
+
+trait FullTextSearch
+{
+	/**
+	 * Replaces spaces with full text search wildcards
+	 *
+	 * @param string $term
+	 * @return string
+	 */
+	protected function fullTextWildcards($term)
+	{
+		// removing symbols used by MySQL
+		$reservedSymbols = ['-', '+', '<', '>', '@', '(', ')', '~'];
+		$term = str_replace($reservedSymbols, '', $term);
+
+		$words = explode(' ', $term);
+
+		foreach($words as $key => $word) {
+			/*
+			 * applying + operator (required word) only big words
+			 * because smaller ones are not indexed by mysql
+			 */
+			if(strlen($word) >= 3) {
+				$words[$key] = '+' . $word . '*';
+			}
+		}
+
+		$searchTerm = implode( ' ', $words);
+
+		return $searchTerm;
+	}
+
+	/**
+	 * Scope a query that matches a full text search of term.
+	 *
+	 * @param \Illuminate\Database\Eloquent\Builder $query
+	 * @param string $term
+	 *
+	 * @return \Illuminate\Database\Eloquent\Builder
+	 */
+	public function scopeSearch($query, $term)
+	{
+		$columns = implode(',', array_map(function($value) {
+			return $this->table . '.' . $value;
+		}, $this->searchable));
+
+		$as = $this->table . '_relevance_score';
+
+		$query->selectRaw("MATCH ({$columns}) AGAINST (? IN BOOLEAN MODE) AS {$as}", [$this->fullTextWildcards($term)])
+		             ->whereRaw("MATCH ({$columns}) AGAINST (? IN BOOLEAN MODE)", [$this->fullTextWildcards($term)])
+		             ->orderByDesc("{$as}");
+
+		return $query;
+	}
+}
