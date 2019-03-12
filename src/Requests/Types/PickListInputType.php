075ade126d0f76e9e9e178d7f389f9cf025c7d44
Medard Ilunga <medard.ilunga@outlook.com>
2019-03-12 15:26:31 +0200

    Allow pick list item options to accept option key and label params

--- src/Requests/Types/PickListInputType.php
+++ src/Requests/Types/PickListInputType.php
@@ -5,26 +5,28 @@ namespace Foundry\Requests\Types;
 
 class PickListInputType extends ChoiceInputType {
 
-	/**
-	 * The input options
-	 *
-	 * @param string $identifier The identifier to get the list items
-	 *
-	 * @return array
-	 */
-	static function list( $identifier ): array {
-		return config('foundry.pick-list-items.model')::query()
-		                   ->select( [ 'pick_list_items.*' ] )
-		                   ->join( 'pick_lists', 'pick_list_items.pick_list_id', '=', 'pick_lists.id' )
-		                   ->where( 'pick_list_items.status', 1 )
-		                   ->where( 'pick_lists.identifier', $identifier )
-		                   ->orderBy( 'pick_list_items.label', 'ASC' )
-		                   ->get()
-		                   ->pluck( 'label', 'id' )
-		                   ->toArray();
-	}
+    /**
+     * The input options
+     *
+     * @param string $identifier The identifier to get the list items
+     *
+     * @param string $key
+     * @param string $label
+     * @return array
+     */
+    static function list( $identifier , $key = 'id', $label='label'): array {
+        return config('foundry.pick-list-items.model')::query()
+            ->select( [ 'pick_list_items.*' ] )
+            ->join( 'pick_lists', 'pick_list_items.pick_list_id', '=', 'pick_lists.id' )
+            ->where( 'pick_list_items.status', 1 )
+            ->where( 'pick_lists.identifier', $identifier )
+            ->orderBy( 'pick_list_items.label', 'ASC' )
+            ->get()
+            ->pluck( $label, $key )
+            ->toArray();
+    }
 
-	static function cast($item)
+    static function cast($item)
 	{
 		if ($item->isMultiple()) {
 			return 'array_int';
