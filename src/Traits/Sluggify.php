c6eb9760a15ead6dbabf186edc15f9da6cbd941b
tichafaraatcofoundry <tichafara@co-foundry.co.za>
2019-05-13 10:33:37 +0200

    Removing special characters(/,\) from slug.

--- src/Traits/Sluggify.php
+++ src/Traits/Sluggify.php
@@ -14,87 +14,94 @@ use Illuminate\Support\Collection;
  * @author Medard Ilunga
  *
  */
-trait Sluggify {
+trait Sluggify
+{
 
 
-	/**
-	 * Get field name representing the slug, default slug
-	 *
-	 * @return string
-	 */
-	public function getSlugField() {
-		return isset( $this->slug_field ) ?
-			$this->slug_field :
-			'slug';
-	}
+    /**
+     * Get field name representing the slug, default slug
+     *
+     * @return string
+     */
+    public function getSlugField()
+    {
+        return isset($this->slug_field) ?
+            $this->slug_field :
+            'slug';
+    }
 
-	/**
-	 * Get field name from which slug is to be created, default name
-	 *
-	 * @return string
-	 */
-	public function getSluggableField() {
-		return isset( $this->sluggable ) ?
-			$this->sluggable :
-			'name';
-	}
+    /**
+     * Get field name from which slug is to be created, default name
+     *
+     * @return string
+     */
+    public function getSluggableField()
+    {
+        return isset($this->sluggable) ? $this->sluggable :
+            'name';
+    }
 
-	/**
-	 * Laravel Model Boot function
-	 */
-	protected static function bootSluggify() {
-		static::creating( function ( $model ) {
-			/**@var $model Sluggify */
-			$model[ $model->getSlugField() ] = $model->createSlug( $model[ $model->getSluggableField() ] );
-		} );
-	}
+    /**
+     * Laravel Model Boot function
+     */
+    protected static function bootSluggify()
+    {
+        static::creating(function ($model) {
+            /**@var $model Sluggify */
+            $model[$model->getSlugField()] = $model->createSlug($model[$model->getSluggableField()]);
+        });
+    }
 
-	/**
-	 * Create a new slug
-	 *
-	 * @param $sluggable
-	 *
-	 * @return string
-	 * @throws \Exception
-	 */
-	public function createSlug( $sluggable ) {
-		//Create slug
-		$slug  = str_slug( $sluggable );
-		$field = $this->getSlugField();
-		// Get any that could possibly be related.
-		$allSlugs = $this->getRelatedSlugs( $slug, $field );
-		// If we haven't used it before then we are all good.
-		/**@var $allSlugs Collection */
-		if ( ! $allSlugs->contains( $field, $slug ) ) {
-			return $slug;
-		}
-		// Just append numbers until we find one not used.
-		for ( $i = 1; $i <= 1000; $i ++ ) {
-			$newSlug = $slug . '-' . $i;
-			if ( ! $allSlugs->contains( $field, $newSlug ) ) {
-				return $newSlug;
-			}
-		}
+    /**
+     * Create a new slug
+     *
+     * @param $sluggable
+     *
+     * @return string
+     * @throws \Exception
+     */
+    public function createSlug($sluggable)
+    {
+        $special = array("/", "\\");
+        $sluggable = str_replace($special, " ", $sluggable);
+        //Create slug
+        $slug = str_slug($sluggable);
+        $field = $this->getSlugField();
+        // Get any that could possibly be related.
+        $allSlugs = $this->getRelatedSlugs($slug, $field);
+        // If we haven't used it before then we are all good.
+        /**@var $allSlugs Collection */
+        if (!$allSlugs->contains($field, $slug)) {
+            return $slug;
+        }
+        // Just append numbers until we find one not used.
+        for ($i = 1; $i <= 1000; $i++) {
+            $newSlug = $slug . '-' . $i;
+            if (!$allSlugs->contains($field, $newSlug)) {
+                return $newSlug;
+            }
+        }
 
-		throw new \Exception( 'Can not create a unique slug' );
-	}
+        throw new \Exception('Can not create a unique slug');
+    }
 
-	/**
-	 * Get models with given slug
-	 *
-	 * @param $slug
-	 * @param $field
-	 *
-	 * @return mixed
-	 */
-	private function getRelatedSlugs( $slug, $field ) {
-		$query = static::withTrashed()->select( $field )->where( $field, 'like', $slug . '%' );
+    /**
+     * Get models with given slug
+     *
+     * @param $slug
+     * @param $field
+     *
+     * @return mixed
+     */
+    private function getRelatedSlugs($slug, $field)
+    {
+        $query = static::withTrashed()->select($field)->where($field, 'like', $slug . '%');
 
-		if ( isset( $this->id ) ) {
-			$query->where( 'id', '!=', $this->id );
-		}
+        if (isset($this->id)) {
+            $query->where('id', '!=', $this->id);
+        }
 
-		return $query->get();
-	}
+        return $query->get();
+    }
 
 }
