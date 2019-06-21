fc55411a5dc62d593b19d4c6ac35010932458b2b
Medard Ilunga <medard.ilunga@outlook.com>
2019-06-21 16:37:20 +0200

    - Update checking of hidden fields to take into consideration relations

--- src/Controllers/Traits/HasBrowseFormRequest.php
+++ src/Controllers/Traits/HasBrowseFormRequest.php
@@ -2,75 +2,148 @@
 
 namespace Foundry\Controllers\Traits;
 
+use Foundry\Models\InputCollection;
 use Foundry\Requests\FormRequest;
 use Foundry\Requests\Types\DocType;
 use Foundry\Requests\Types\FormType;
 use Foundry\Requests\Types\SubmitButtonType;
 use Illuminate\Http\Request;
+use Plugins\Foundry\System\Policies\HasFieldPermission;
 
 trait HasBrowseFormRequest {
 
-	/**
-	 * Bind data to the view.
-	 *
-	 * @param string $class The FormQuest class to use
-	 * @param Request $request The current request
-	 *
-	 * @return array
-	 */
-	public function handleBrowseRequest( string $class, Request $request ) : array
-	{
-		/**
-		 * @var FormRequest $class
-		 */
-		$response = $class::handleRequest( $request );
+    /**
+     * Bind data to the view.
+     *
+     * @param string $class The FormQuest class to use
+     * @param Request $request The current request
+     *
+     * @return array
+     */
+    public function handleBrowseRequest( string $class, Request $request ) : array
+    {
+        /**
+         * @var FormRequest $class
+         */
+        $response = $class::handleRequest( $request );
 
-		if ( $response->isSuccess() ) {
-			return $this->handleBrowseResponse( $class, $request, $response );
-		} else {
-			abort( $response->getCode(), $response->getMessage() );
-		}
-	}
+        if ( $response->isSuccess() ) {
+            return $this->handleBrowseResponse( $class, $request, $response );
+        } else {
+            abort( $response->getCode(), $response->getMessage() );
+        }
+    }
 
-	/**
-	 * @param string $class
-	 * @param Request $request
-	 * @param $response
-	 *
-	 * @return array
-	 */
-	protected function handleBrowseResponse( string $class, Request $request, $response = null ): array {
-		/**
-		 * @var FormType $form
-		 */
-		$form = $class::form( $request, null );
-		$doctype = null;
+    /**
+     * @param string $class
+     * @param Request $request
+     * @param $response
+     *
+     * @return array
+     */
+    protected function handleBrowseResponse( string $class, Request $request, $response = null ): array {
+        /**
+         * @var FormType $form
+         */
+        $form = $class::form( $request, null );
+        $doctype = null;
 
-		$data = [];
+        $data = [];
 
-		if ($inputs = $form->getInputs()) {
-			$form
-				->setMethod( 'GET' )
-				->setButtons(
-					( new SubmitButtonType( __( 'Filter' ), $form->getAction() ) )
-					//,( new ResetButtonType( __( 'Reset' ), $form->getAction() ) )
-				);
+        if ($inputs = $form->getInputs()) {
+            $form
+                ->setMethod( 'GET' )
+                ->setButtons(
+                    ( new SubmitButtonType( __( 'Filter' ), $form->getAction() ) )
+                //,( new ResetButtonType( __( 'Reset' ), $form->getAction() ) )
+                );
 
-			if ( $response && ! $response->isSuccess() ) {
-				$form->setErrors( $response->getError() );
-			}
-			$form->addChildren(...array_values($inputs));
-			$doctype = DocType::withChildren( $form );
-		}
+            if ( $response && ! $response->isSuccess() ) {
+                $form->setErrors( $response->getError() );
+            }
+            $form->addChildren(...array_values($inputs));
+            $doctype = DocType::withChildren( $form );
+        }
 
-		$data['form'] = $doctype;
-		$data['data'] = ($response) ? $response->getData() : null;
+        $data['form'] = $doctype;
+        $data['data'] = ($response) ? $response->getData() : null;
 
-		if ( method_exists( $class, 'columns' ) ) {
-			$data['columns'] = $class::columns();
-		}
+        if ( method_exists( $class, 'columns' ) ) {
 
-		return $data;
-	}
+            /**
+             * @var $collections InputCollection
+             */
+            $collections = $class::columns();
+            $allowed = $collections->keys()->toArray();
+
+            $serviceClass = $class::service();
+
+            if($serviceClass && class_exists($serviceClass)){
+                $modelClass = (new $serviceClass())->modelClass();
+
+                if($modelClass && class_exists($modelClass)){
+
+                    $model = new $modelClass();
+
+                    if(key_exists(HasFieldPermission::class, class_uses($model))){
+
+                        $diff = array_diff($allowed, $model->getHidden());
+
+                        $allowed = array();
+
+                        foreach ($diff as $key){
+
+                            $relations = explode('.', $key);
+                            $add = true;
+
+                            if(sizeof($relations) > 1 &&
+                                isset($model->permissionRelations()[$relations[0]])){
+
+                                $hide = $model->getHidden();
+
+                                for ($i = 0, $j = 1; $j < sizeof($relations); $i++, $j++){
+
+                                    if($add){
+                                        $hidden = in_array($relations[$i], $hide);
+
+                                        $c = $model->permissionRelations()[$relations[$i]];
+
+                                        if(isset($c['class'])){
+
+                                            $relatedClass = $c['class'];
+
+                                            $relatedModel = new $relatedClass();
+
+                                            if(!$hidden && key_exists(HasFieldPermission::class, class_uses($relatedModel))){
+
+                                                if(in_array($relations[$j], $relatedModel->getHidden())){
+                                                    $add = false;
+                                                }
+
+                                            }else{
+                                                $add = !$hidden;
+                                            }
+                                        }
+
+                                    }
+
+                                }
+
+                            }
+
+                            if($add){
+                                array_push($allowed, $key);
+                            }
+
+                        }
+                    }
+                }
+            }
+
+            $data['columns'] = $collections->only($allowed);
+        }
+
+        return $data;
+    }
 
 }
--- src/Requests/Types/Traits/HasModel.php
+++ src/Requests/Types/Traits/HasModel.php
@@ -6,66 +6,102 @@ use Illuminate\Database\Eloquent\Model;
 
 trait HasModel {
 
-	/**
-	 * @var Model
-	 */
-	protected $model;
+    /**
+     * @var Model
+     */
+    protected $model;
 
-	/**
-	 * @return Model
-	 */
-	public function getModel(): Model {
-		return $this->model;
-	}
+    /**
+     * @return Model
+     */
+    public function getModel(): Model {
+        return $this->model;
+    }
 
-	/**
-	 * @param Model|null $model
-	 *
-	 * @return $this
-	 */
-	public function setModel( Model &$model = null ) {
-		$this->model = $model;
+    /**
+     * @param Model|null $model
+     *
+     * @return $this
+     */
+    public function setModel( Model &$model = null ) {
+        $this->model = $model;
 
-		return $this;
-	}
+        return $this;
+    }
 
-	/**
-	 * @return bool
-	 */
-	public function hasModel(): bool {
-		return ! ! ( $this->model );
-	}
+    /**
+     * @return bool
+     */
+    public function hasModel(): bool {
+        return ! ! ( $this->model );
+    }
 
-	public function isFillable() {
-		if ( $this->hasModel() ) {
-			return $this->getModel()->isFillable( $this->getName() );
-		}
+    public function isFillable() {
+        if ( $this->hasModel() ) {
+            return $this->getModel()->isFillable( $this->getName() );
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	public function isVisible() {
-		if ( $this->hasModel() ) {
-			$hidden  = $this->getModel()->getHidden();
-			$visible = $this->getModel()->getVisible();
-			if ( ! in_array( $this->getName(), $hidden ) && in_array( $this->getName(), $visible ) ) {
-				return true;
-			} elseif ( in_array( $this->getName(), $hidden ) ) {
-				return false;
-			}
-		}
+    public function isVisible() {
+        if ( $this->hasModel() ) {
+            $hidden  = $this->getModel()->getHidden();
+            $visible = $this->getModel()->getVisible();
+            if ( ! $this->isHidden() && in_array( $this->getName(), $visible ) ) {
+                return true;
+            } elseif ( in_array( $this->getName(), $hidden ) ) {
+                return false;
+            }
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	public function isHidden() {
-		if ( $this->hasModel() ) {
-			$hidden = $this->getModel()->getHidden();
-			if ( in_array( $this->getName(), $hidden ) ) {
-				return true;
-			}
-		}
-		return false;
-	}
+    public function isHidden() {
+
+        if ( $this->hasModel() ) {
+
+            $hidden = $this->getModel()->getHidden();
+
+            if(strpos($this->getName(), '.') > 0){
+
+                $annotations = explode('.', $this->getName());
+                $relations = $this->getModel()->getRelations();
+
+                return $this->hisRelatedFieldHidden($hidden, $annotations, $relations);
+
+            }else{
+                if ( in_array( $this->getName(), $hidden ) ) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private function hisRelatedFieldHidden(array $hidden, array $annotations, array $relations)
+    {
+        $hide = false;
+
+        if(isset($relations[$annotations[0]])){
+
+            for ($i = 0, $j = 1; $j < sizeof($annotations); $i++, $j++){
+
+                $hide = in_array($annotations[$i], $hidden);
+
+                if(!$hide){
+                    $related = $relations[$annotations[$i]];
+                    $hidden = $related->getHidden();
+                    $hide = in_array($annotations[$j], $hidden);
+                }
+
+            }
+
+        }
+
+        return $hide;
+    }
 
 }
